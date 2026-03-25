48614a055aa38a0886d29610869ec24ccf669296
Tanjin <NamelessKing@users.noreply.github.com>
2026-03-25 15:41:28 +0100

    fix

--- src/test/java/com/rentalops/RentalopsBackendApplicationTests.java
+++ src/test/java/com/rentalops/RentalopsBackendApplicationTests.java
@@ -3,9 +3,13 @@ package com.rentalops;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
+@ActiveProfiles("test")
+@TestPropertySource(properties = "spring.config.import=")
 class RentalopsBackendApplicationTests {
 
 	@Test
